06273ae89ba41cd962e564306b587a117ff6ed04
mochrks <mochrizkiks@gmail.com>
2024-09-11 12:31:32 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Button>Tes ini button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
